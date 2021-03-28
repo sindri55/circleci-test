b714712574e3eaff4b7eba56b1f6fdf69e3f36c9
Sindri <55@55.is>
2021-03-28 21:13:09 +0200

    modify app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('fooooo barrrrr');
+  res.write('foooooooo barrrrr');
   res.end();
 }).listen(8080, '0.0.0.0');
